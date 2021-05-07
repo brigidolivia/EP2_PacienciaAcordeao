a112cae599dd05535d90e60146ef9170f94d2883
brigidolivia <livia.outreach@gmail.com>
2021-05-07 01:05:27 -0300

    Refacção de métodos + correção de indice de visualização

--- EP2/jogo.py
+++ EP2/jogo.py
@@ -1,10 +1,11 @@
 from funcoes import *
+from design import *
 input('Quer jogar?')
 mesa= cria_baralho()
 pare=False
 while not pare:
-    print(mesa)
-    cartaescolhida=try_input("Qual posição a sua carta escolhida está?")
+    print_estilizado(mesa)
+    cartaescolhida=peganumerodacarta("Qual posição a sua carta escolhida está?")
     if mesa ==1:
         r= input ("Você ganhou, bora de novo?(s/n")
         if r=='s':
@@ -25,15 +26,15 @@ while not pare:
      
     #Acho que essa parte pode virar função
     while movimentos==[]:
-        cartaescolhida= try_input('Vish, carta inválida. Escolha outra:')
+        cartaescolhida= peganumerodacarta('Vish, carta inválida. Escolha outra:')
         movimentos= lista_movimentos_possiveis(mesa, cartaescolhida)
     
     if movimentos==[1,3]:
         anterior=cartaescolhida-1
         terceira_anterior=cartaescolhida-3
-        posicao_escolhida= try_input("Duas possibilidades! {} ou {}?".format(anterior, terceira_anterior))
+        posicao_escolhida= peganumerodacarta("Duas possibilidades! {} ou {}?".format((anterior+1), (terceira_anterior+1)))
         while posicao_escolhida!=anterior and posicao_escolhida!=terceira_anterior:
-            posicao_escolhida= try_input("Inválido. Só duas possibilidades! {} ou {}?".format(anterior, terceira_anterior))
+            posicao_escolhida= peganumerodacarta("Inválido. Só duas possibilidades! {} ou {}?".format(anterior+1, terceira_anterior+1))
         if posicao_escolhida==anterior:
             mesa= empilha(mesa, cartaescolhida, anterior)
         elif posicao_escolhida==terceira_anterior:
