70b81efba9480970362a33c0262cdef938b4a5c2
brigidolivia <livia.outreach@gmail.com>
2021-05-09 03:32:53 -0300

    ÚLTIMO DESIGN- Cartinhas Brancas! +Cartas originais diminuídas +Colocado o Back.White nas cartinhas +O amarele ficou ruim, mudei para magenta

--- EP2/design.py
+++ EP2/design.py
@@ -15,23 +15,28 @@ def print_estilizado(mesa):
             cor_corrente = Fore.RED
         
         else:
-            cor_corrente = Fore.YELLOW
+            cor_corrente = Fore.MAGENTA
         #Três aspas== Conseguir escrever em multilinhas
         #ljust(quantascasas)= Imprimir certa quantidade de casas e preencher com espaços se faltar (à direita)
         print(
-            """
-{0}{1}.{2}
-┌─────────┐
-│{3}░░░░░░░│
-│░░░░░░░░░│
-│░░░░░░░░░│
-│░░░{4}░░░░░│
-│░░░░░░░░░│
-│░░░░░░░░░│
-│░░░░░░░{3}│
-└─────────┘
 """
-                  .format((Fore.WHITE),(i+1),(cor_corrente),(extrai_valor(mesa[i]).ljust(2)),(extrai_naipe(mesa[i]))))
+{0}{1}.{2}
+{5}┌──────┐{6}
+{5}│{3}░░░░│{6}
+{5}│░░{4}░░░│{6}
+{5}│░░░░{3}│{6}
+{5}└──────┘{6}
+"""
+        .format(
+            (Fore.WHITE),
+            (i+1),
+            (cor_corrente),
+            (extrai_valor(mesa[i]).ljust(2)),
+            (extrai_naipe(mesa[i])),
+            (Back.WHITE),
+            (Back.BLACK)
+            )
+        ,end="")  #end== Todo print tem como açao final uma quebra de linha (\n- "new line"), mas agora, definindo o end, eu pude tirar uma quebra de linha=== Cartas mais próximas!
     print(Fore.WHITE)
     return
 
