18ad570fb49df1bf8a01745af65145e092c2e8d2
brigidolivia <livia.outreach@gmail.com>
2021-05-09 03:02:40 -0300

    Análise das possibilidades colocada no fim +Só percebi quando tinha só duas cartas +Transformação de 3 prints em um só

--- EP2/jogo.py
+++ EP2/jogo.py
@@ -16,22 +16,7 @@ pare=False
 while not pare:
     print_estilizado(mesa)
     cartaescolhida=peganumerodacarta("Qual posição a sua carta escolhida está?", mesa)
-    if mesa ==1:
-        r= input ("Você ganhou, bora de novo?(s/n")
-        if r=='s':
-            pare=False
-        else:
-            print("Beleza! Talvez numa próxima!")
-            pare=True
-
-    elif possui_movimentos_possiveis(mesa)==0:
-        r= input ("Você perdeu, aceita revanche?(s/n)")
-        if r=='s':
-            pare=False
-        else:
-            print("Beleza! Talvez numa próxima!")
-            pare=True
-
+    
     movimentos= lista_movimentos_possiveis(mesa, cartaescolhida)
      
     #Acho que essa parte pode virar função
@@ -59,6 +44,26 @@ while not pare:
         terceira_anterior= cartaescolhida-3
         mesa= empilha(mesa, cartaescolhida, terceira_anterior)
 
-print("Bem, finalizamos por aqui. Se jogar não foi o suficiente, aqui vai uma curiosidade!:")
-print("Você sabia que uma das vertentes da origem do baralho diz que o jogo era a representação das classes sociais na França quando Carlos VI ainda era Rei?")
-print("Agora é trabalho teu descobrir qual naipe representava a burguesia, o clero, os militares e os camponeses! Fui :) ")
+    
+    if len(mesa) ==1:
+        r= input ("Você ganhou, bora de novo?(s/n)")
+        if r=='s':
+            mesa= cria_baralho()
+            pare=False
+        else:
+            print("Beleza! Talvez numa próxima!")
+            pare=True
+    elif possui_movimentos_possiveis(mesa)==0:
+        r= input ("Você perdeu, aceita revanche?(s/n)")
+        if r=='s':
+            mesa= cria_baralho()
+            pare=False
+        else:
+            print("Beleza! Talvez numa próxima!")
+            pare=True
+
+print(grifa("""
+Bem, finalizamos por aqui. Se jogar não foi o suficiente, aqui vai uma curiosidade!:
+Você sabia que uma das vertentes da origem do baralho diz que o jogo era a representação das classes sociais na França quando Carlos VI ainda era Rei?
+Agora é trabalho teu descobrir qual naipe representava a burguesia, o clero, os militares e os camponeses! Fui :) 
+"""))
